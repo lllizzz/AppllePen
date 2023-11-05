5fa703ee2b27f49668c45559dbb90e570e65e898
Elizaveta <elizaveta.shetkina@gmail.com>
2023-11-05 23:36:39 +0300

    ApplePen

---
+++ ApplePen.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<char> letters;
+    string word1;
+    string word;
+    cin >> word1;
+    for (char i : word1)
+    {
+        if (!letters.contains(i))
+        {
+            letters.insert(i);
+        }
+    }
+    while (cin >> word)
+    {
+        for (auto letter : letters)
+        {
+            if (word.find(letter) == std::string::npos)
+            {
+                letters.erase(letter);
+            }
+        }
+    }
+    for (auto letter : letters)
+    {
+        cout << letter;
+    }
+}
